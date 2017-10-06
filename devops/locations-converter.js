ea1082bac8236b66c0bf23cbfd431daf7b6bae40
Guy Ellis <wildfiction@gmail.com>
2017-10-06 09:16:47 -0700

    Remove async module from dependencies. Issue #1320

--- devops/locations-converter.js
+++ devops/locations-converter.js
@@ -39,48 +39,48 @@
 //   },
 // };
 
-const mongo = require('../lib/db/mongo');
-const async = require('async');
-const Logger = require('../lib/logging/logger');
+// const mongo = require('../lib/db/mongo');
+// const async = require('async');
+// const Logger = require('../lib/logging/logger');
 
-/* eslint-disable no-param-reassign, no-console */
+// /* eslint-disable no-param-reassign, no-console */
 
-if (!process.env.DEBUG) {
-  console.log('Set DEBUG env to "plant:*" before running...');
-  process.exit(1);
-}
+// if (!process.env.DEBUG) {
+//   console.log('Set DEBUG env to "plant:*" before running...');
+//   process.exit(1);
+// }
 
-Logger.setLevel('trace');
-const logger = new Logger('devops-location-converter');
+// Logger.setLevel('trace');
+// const logger = new Logger('devops-location-converter');
 
-mongo.queryByCollection('location', {}, (allLocationsErr, locations) => {
-  if (allLocationsErr) {
-    logger.trace('allLocationsErr', { allLocationsErr });
-    return;
-  }
+// mongo.queryByCollection('location', {}, (allLocationsErr, locations) => {
+//   if (allLocationsErr) {
+//     logger.trace('allLocationsErr', { allLocationsErr });
+//     return;
+//   }
 
-  logger.trace(`About to process ${locations.length} locations`);
+//   logger.trace(`About to process ${locations.length} locations`);
 
-  async.each(locations, (location, cb) => {
-    location.createdBy = location.userId;
-    delete location.userId;
+//   async.each(locations, (location, cb) => {
+//     location.createdBy = location.userId;
+//     delete location.userId;
 
-    location.members = {};
-    location.userIds.forEach((userId) => {
-      location.members[userId.id.toString()] = userId.role;
-    });
-    delete location.userIds;
+//     location.members = {};
+//     location.userIds.forEach((userId) => {
+//       location.members[userId.id.toString()] = userId.role;
+//     });
+//     delete location.userIds;
 
-    location.stations = {};
+//     location.stations = {};
 
-    mongo.replaceDocument('location', location, cb);
-  }, (asyncEachLocationErr) => {
-    if (asyncEachLocationErr) {
-      logger.trace('asyncEachLocationErr', { asyncEachLocationErr });
-    } else {
-      logger.trace('All complete');
-    }
-    logger.trace('Closing...');
-    mongo._close();
-  });
-});
+//     mongo.replaceDocument('location', location, cb);
+//   }, (asyncEachLocationErr) => {
+//     if (asyncEachLocationErr) {
+//       logger.trace('asyncEachLocationErr', { asyncEachLocationErr });
+//     } else {
+//       logger.trace('All complete');
+//     }
+//     logger.trace('Closing...');
+//     mongo._close();
+//   });
+// });
--- devops/reload-images-s3.js
+++ devops/reload-images-s3.js
@@ -3,143 +3,144 @@
 
 // require('babel-core/register');
 
-const Logger = require('../lib/logging/logger');
-const _ = require('lodash');
-const async = require('async');
-const AWS = require('aws-sdk');
-const constants = require('../app/libs/constants');
-const mongo = require('../lib/db/mongo');
+// const Logger = require('../lib/logging/logger');
+// const _ = require('lodash');
+// const async = require('async');
+// const AWS = require('aws-sdk');
+// const constants = require('../app/libs/constants');
+// const mongo = require('../lib/db/mongo');
 
-Logger.setLevel('trace');
-const logger = Logger.create('plant:reload-images');
-const s3 = new AWS.S3();
-const Bucket = 'i.plaaant.com';
-const Prefix = 'up/orig';
+// Logger.setLevel('trace');
+// const logger = Logger.create('plant:reload-images');
+// const s3 = new AWS.S3();
+// const Bucket = 'i.plaaant.com';
+// const Prefix = 'up/orig';
 
-function getListOfKeys(cb) {
-  logger.trace('getListOfKeys() start');
-  const params = { Bucket, Prefix };
-  s3.listObjectsV2(params, (listObjectsErr, objects) => {
-    if (listObjectsErr) {
-      logger.error('listObjectsV2', { listObjectsErr });
-    }
-    /* data: { IsTruncated: false,
-    Contents:
-    [ { Key: 'test/orig/2016-09-15 19.10.00.jpg',
-        LastModified: 2016-09-16T19:58:40.000Z,
-        ETag: '"5291ce2b140317adfd5dc44e"',
-        Size: 3494653,
-        StorageClass: 'STANDARD' },
-        ...
-    ],
-    Name: 'i.plaaant.com',
-    Prefix: 'test/orig',
-    MaxKeys: 1000,
-    CommonPrefixes: [],
-    KeyCount: 4 }
-  */
-    const keys = objects.Contents.map(content => content.Key);
-    logger.trace('Number of keys', { keyCount: keys.length });
-    cb(listObjectsErr, keys);
-  });
-}
+// function getListOfKeys(cb) {
+//   logger.trace('getListOfKeys() start');
+//   const params = { Bucket, Prefix };
+//   s3.listObjectsV2(params, (listObjectsErr, objects) => {
+//     if (listObjectsErr) {
+//       logger.error('listObjectsV2', { listObjectsErr });
+//     }
+//     /* data: { IsTruncated: false,
+//     Contents:
+//     [ { Key: 'test/orig/2016-09-15 19.10.00.jpg',
+//         LastModified: 2016-09-16T19:58:40.000Z,
+//         ETag: '"5291ce2b140317adfd5dc44e"',
+//         Size: 3494653,
+//         StorageClass: 'STANDARD' },
+//         ...
+//     ],
+//     Name: 'i.plaaant.com',
+//     Prefix: 'test/orig',
+//     MaxKeys: 1000,
+//     CommonPrefixes: [],
+//     KeyCount: 4 }
+//   */
+//     const keys = objects.Contents.map(content => content.Key);
+//     logger.trace('Number of keys', { keyCount: keys.length });
+//     cb(listObjectsErr, keys);
+//   });
+// }
 
-function getImageIds(keys, cb) {
-  logger.trace('getImageIds() start');
-  const filenames = keys.map((key) => {
-    const parts = key.split('/');
-    return {
-      key,
-      imageId: _.last(parts).split('.')[0],
-    };
-  });
-  logger.trace(`${filenames.length} keys before filtering`);
+// function getImageIds(keys, cb) {
+//   logger.trace('getImageIds() start');
+//   const filenames = keys.map((key) => {
+//     const parts = key.split('/');
+//     return {
+//       key,
+//       imageId: _.last(parts).split('.')[0],
+//     };
+//   });
+//   logger.trace(`${filenames.length} keys before filtering`);
 
-  const imageIds = filenames.filter(fn => constants.mongoIdRE.test(fn.imageId));
-  logger.trace(`${imageIds.length} image ids after filtering`);
+//   const imageIds = filenames.filter(fn => constants.mongoIdRE.test(fn.imageId));
+//   logger.trace(`${imageIds.length} image ids after filtering`);
 
-  cb(null, imageIds);
-}
+//   cb(null, imageIds);
+// }
 
-// data is an array. Each object in array:
-//   key, imageId
-function getMetadata(data, cb) {
-  logger.trace('getMetadata() start');
-  async.map(data, (item, done) => {
-    mongo.getNoteByImageId(item.imageId, (getNoteByImageIdErr, note) => {
-      if (getNoteByImageIdErr) {
-        logger.error('Error getting note:', { getNoteByImageIdErr });
-        return done(getNoteByImageIdErr);
-      }
-      if (!note.images || !note.images.length) {
-        logger.error('No images in note:', { note });
-        return done('No images');
-      }
-      const image = note.images.find(img => img.id === item.imageId);
-      if (!image) {
-        logger.error('Image not found in note', { imageId: item.imageId, 'note.images': note.images });
-        return done('No image');
-      }
-      return done(null, {
-        Key: item.key,
-        Metadata: {
-          userid: note.userId,
-          id: image.id,
-          noteid: note._id,
-          originalname: image.originalname,
-        },
-      });
-    });
-  }, (asyncMapError, results) => {
-    if (asyncMapError) {
-      logger.error('asyncMapError in getMetadata', { asyncMapError });
-    }
-    cb(asyncMapError, results);
-  });
-}
+// // data is an array. Each object in array:
+// //   key, imageId
+// function getMetadata(data, cb) {
+//   logger.trace('getMetadata() start');
+//   async.map(data, (item, done) => {
+//     mongo.getNoteByImageId(item.imageId, (getNoteByImageIdErr, note) => {
+//       if (getNoteByImageIdErr) {
+//         logger.error('Error getting note:', { getNoteByImageIdErr });
+//         return done(getNoteByImageIdErr);
+//       }
+//       if (!note.images || !note.images.length) {
+//         logger.error('No images in note:', { note });
+//         return done('No images');
+//       }
+//       const image = note.images.find(img => img.id === item.imageId);
+//       if (!image) {
+//         logger.error('Image not found in note', 
+//            { imageId: item.imageId, 'note.images': note.images });
+//         return done('No image');
+//       }
+//       return done(null, {
+//         Key: item.key,
+//         Metadata: {
+//           userid: note.userId,
+//           id: image.id,
+//           noteid: note._id,
+//           originalname: image.originalname,
+//         },
+//       });
+//     });
+//   }, (asyncMapError, results) => {
+//     if (asyncMapError) {
+//       logger.error('asyncMapError in getMetadata', { asyncMapError });
+//     }
+//     cb(asyncMapError, results);
+//   });
+// }
 
-// data is an array with {Key, Metadata}
-function resaveImage(data, cb) {
-  logger.trace('resaveImage() start', { data });
-  // Slow this part down by running in series
-  async.mapSeries(data, (item, done) => {
-    const { Key, Metadata } = item;
-    const getParams = { Key, Bucket };
-    s3.getObject(getParams, (getObjectErr, getObjectResult) => {
-      if (getObjectErr) {
-        return done(getObjectErr);
-      }
-      const putParams = {
-        Key: Key.replace('up', 'test'),
-        Body: getObjectResult.Body,
-        Bucket,
-        Metadata,
-      };
-      logger.trace('About to put object', { putParams });
-      return s3.putObject(putParams, (err, result) => {
-        logger.trace('PUT complete, wait for 1 second...', { Key });
-        // Pause at this point to let server keep up with Lambda requests
-        setTimeout(() => { done(err, result); }, 1000);
-      });
-    });
-  }, (asyncMapError, results) => {
-    if (asyncMapError) {
-      logger.error('resaveImage async.map', { asyncMapError });
-    }
-    return cb(asyncMapError, results);
-  });
-}
+// // data is an array with {Key, Metadata}
+// function resaveImage(data, cb) {
+//   logger.trace('resaveImage() start', { data });
+//   // Slow this part down by running in series
+//   async.mapSeries(data, (item, done) => {
+//     const { Key, Metadata } = item;
+//     const getParams = { Key, Bucket };
+//     s3.getObject(getParams, (getObjectErr, getObjectResult) => {
+//       if (getObjectErr) {
+//         return done(getObjectErr);
+//       }
+//       const putParams = {
+//         Key: Key.replace('up', 'test'),
+//         Body: getObjectResult.Body,
+//         Bucket,
+//         Metadata,
+//       };
+//       logger.trace('About to put object', { putParams });
+//       return s3.putObject(putParams, (err, result) => {
+//         logger.trace('PUT complete, wait for 1 second...', { Key });
+//         // Pause at this point to let server keep up with Lambda requests
+//         setTimeout(() => { done(err, result); }, 1000);
+//       });
+//     });
+//   }, (asyncMapError, results) => {
+//     if (asyncMapError) {
+//       logger.error('resaveImage async.map', { asyncMapError });
+//     }
+//     return cb(asyncMapError, results);
+//   });
+// }
 
-logger.trace('Start process...');
+// logger.trace('Start process...');
 
-async.waterfall([
-  getListOfKeys,
-  getImageIds,
-  getMetadata,
-  resaveImage,
-], (waterfallErr, data) => {
-  logger.trace('Waterfall completed', { waterfallErr, data });
-});
+// async.waterfall([
+//   getListOfKeys,
+//   getImageIds,
+//   getMetadata,
+//   resaveImage,
+// ], (waterfallErr, data) => {
+//   logger.trace('Waterfall completed', { waterfallErr, data });
+// });
 
 
 // function getMetadata(data, cb) {
