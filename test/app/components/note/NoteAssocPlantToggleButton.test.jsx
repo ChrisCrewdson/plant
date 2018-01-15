15fccd80274c963b97b2b57183333159f771357a
Guy Ellis <wildfiction@gmail.com>
2018-01-15 09:19:35 -0700

    Add tests for NoteAssocPlantToggleButton. Issue #7

---
+++ test/app/components/note/NoteAssocPlantToggleButton.test.jsx
@@ -0,0 +1,49 @@
+
+const React = require('react');
+const App = require('../../../../app/components/App');
+const store = require('../../../../app/store');
+const NoteAssocPlantToggleButton = require('../../../../app/components/note/NoteAssocPlantToggleButton');
+const renderer = require('react-test-renderer');
+const MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default;
+const getMuiTheme = require('material-ui/styles/getMuiTheme').default;
+const lightBaseTheme = require('material-ui/styles/baseThemes/lightBaseTheme').default;
+const {
+  MemoryRouter,
+} = require('react-router-dom');
+const { Provider } = require('react-redux');
+
+const muiTheme = getMuiTheme(lightBaseTheme);
+
+describe('NoteAssocPlantToggleButton', () => {
+  const storeDispatch = store.dispatch;
+
+  beforeAll(() => {
+    store.dispatch = () => {};
+  });
+
+  afterAll(() => {
+    store.dispatch = storeDispatch;
+  });
+
+  test('NoteAssocPlantToggleButton should be rendered', () => {
+    const component = renderer.create(
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Provider store={store}>
+          <App>
+            <MemoryRouter>
+              <NoteAssocPlantToggleButton
+                _id="buttonId"
+                label="Button Label"
+                primary
+                secondary={false}
+                style={{ color: 'blue' }}
+                toggleFunc={() => {}}
+              />
+            </MemoryRouter>
+          </App>
+        </Provider>
+      </MuiThemeProvider>);
+    const badgeTotals = component.toJSON();
+    expect(badgeTotals).toMatchSnapshot();
+  });
+});
