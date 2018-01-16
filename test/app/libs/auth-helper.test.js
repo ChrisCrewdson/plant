6c656e72437e60eb53ec99ac5f6490925fb22b5e
Guy Ellis <wildfiction@gmail.com>
2018-01-15 21:17:20 -0700

    Complete test coverage for auth-helper. Issue #7

--- test/app/libs/auth-helper.test.js
+++ test/app/libs/auth-helper.test.js
@@ -2,6 +2,41 @@ const authHelper = require('../../../app/libs/auth-helper');
 const seamless = require('seamless-immutable').static;
 
 describe('/app/libs/auth-helper', () => {
+  describe('isOwner', () => {
+    test('should return true with matching userIds', () => {
+      const store = {
+        getState: () => ({ user: { jwt: true, _id: 'u-1' } }),
+      };
+      const obj = {
+        userId: 'u-1',
+      };
+      const owner = authHelper.isOwner(obj, store);
+      expect(owner).toBe(true);
+    });
+
+    test('should return true with non-matching userIds and missing _id', () => {
+      const store = {
+        getState: () => ({ user: { jwt: true, _id: 'u-2' } }),
+      };
+      const obj = {
+        userId: 'u-1',
+      };
+      const owner = authHelper.isOwner(obj, store);
+      expect(owner).toBe(true);
+    });
+
+    test('should return false if no user object on store', () => {
+      const store = {
+        getState: () => ({ }),
+      };
+      const obj = {
+        userId: 'u-1',
+      };
+      const owner = authHelper.isOwner(obj, store);
+      expect(owner).toBe(false);
+    });
+  });
+
   describe('canEdit', () => {
     test('should return false if loggedInUserId is falsy', () => {
       expect(authHelper.canEdit()).toBe(false);
@@ -50,6 +85,13 @@ describe('/app/libs/auth-helper', () => {
         getState: () => seamless.from({
           user: {},
         }),
+      };
+      expect(authHelper.isLoggedIn(store)).toBe(false);
+    });
+
+    test('should return false if user is missing from state', () => {
+      const store = {
+        getState: () => seamless.from({}),
       };
       expect(authHelper.isLoggedIn(store)).toBe(false);
     });
