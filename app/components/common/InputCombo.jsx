6c5a9a9b23928015a32c3d9ed3ee2acd31f8a4ec
Guy Ellis <wildfiction@gmail.com>
2018-01-16 22:19:33 -0700

    Factor InputComboText out of InputCombo. Issue #7

--- app/components/common/InputCombo.jsx
+++ app/components/common/InputCombo.jsx
@@ -2,49 +2,25 @@ const MenuItem = require('material-ui/MenuItem').default;
 const PropTypes = require('prop-types');
 const React = require('react');
 const SelectField = require('material-ui/SelectField').default;
-const TextField = require('material-ui/TextField').default;
+const inputComboText = require('./InputComboText');
 const Toggle = require('material-ui/Toggle').default;
 
 function inputCombo(props = {}) {
   const {
     changeHandler,
-    disabled = false,
     error,
-    fullWidth = true,
     id,
     label,
-    multiLine = false,
     name: namo,
     options,
-    placeholder,
     style = {},
     type = 'text',
     value,
   } = props;
 
-  const underlineStyle = {
-    display: 'none',
-  };
-
   const styler = Object.assign({
     marginLeft: 20,
   }, style);
-
-  const text = () => (<TextField
-    disabled={disabled}
-    errorText={error}
-    floatingLabelText={label}
-    fullWidth={fullWidth}
-    hintText={placeholder}
-    id={id}
-    multiLine={multiLine}
-    name={namo}
-    onChange={changeHandler}
-    style={styler}
-    type={type}
-    underlineStyle={underlineStyle}
-    value={value}
-  />);
 
   const boolean = () => (<Toggle
     toggled={value}
@@ -71,7 +47,7 @@ function inputCombo(props = {}) {
   switch (type) {
     case 'text':
     case 'number':
-      return text();
+      return inputComboText(props);
     case 'boolean':
       return boolean();
     case 'select':
@@ -83,15 +59,11 @@ function inputCombo(props = {}) {
 
 inputCombo.propTypes = {
   changeHandler: PropTypes.func.isRequired,
-  disabled: PropTypes.bool,
   error: PropTypes.string,
-  fullWidth: PropTypes.bool,
   id: PropTypes.string.isRequired,
   label: PropTypes.string,
-  multiLine: PropTypes.bool,
   name: PropTypes.string.isRequired,
   options: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-  placeholder: PropTypes.string,
   // eslint-disable-next-line react/forbid-prop-types
   style: PropTypes.object,
   value: PropTypes.oneOfType([
@@ -103,13 +75,9 @@ inputCombo.propTypes = {
 };
 
 inputCombo.defaultProps = {
-  disabled: false,
   error: '',
-  fullWidth: true,
   label: '',
-  multiLine: false,
   options: {},
-  placeholder: '',
   style: {},
   type: 'text',
 };
---
+++ app/components/common/InputComboText.jsx
@@ -0,0 +1,77 @@
+const PropTypes = require('prop-types');
+const React = require('react');
+const TextField = require('material-ui/TextField').default;
+
+function inputComboText(props = {}) {
+  const {
+    changeHandler,
+    disabled = false,
+    error,
+    fullWidth = true,
+    id,
+    label,
+    multiLine = false,
+    name: namo,
+    placeholder,
+    style = {},
+    type = 'text',
+    value,
+  } = props;
+
+  const underlineStyle = {
+    display: 'none',
+  };
+
+  const styler = Object.assign({
+    marginLeft: 20,
+  }, style);
+
+  return (<TextField
+    disabled={disabled}
+    errorText={error}
+    floatingLabelText={label}
+    fullWidth={fullWidth}
+    hintText={placeholder}
+    id={id}
+    multiLine={multiLine}
+    name={namo}
+    onChange={changeHandler}
+    style={styler}
+    type={type}
+    underlineStyle={underlineStyle}
+    value={value}
+  />);
+}
+
+inputComboText.propTypes = {
+  changeHandler: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  error: PropTypes.string,
+  fullWidth: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  multiLine: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  // eslint-disable-next-line react/forbid-prop-types
+  style: PropTypes.object,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.bool,
+  ]).isRequired,
+  type: PropTypes.string,
+};
+
+inputComboText.defaultProps = {
+  disabled: false,
+  error: '',
+  fullWidth: true,
+  label: '',
+  multiLine: false,
+  placeholder: '',
+  style: {},
+  type: 'text',
+};
+
+module.exports = inputComboText;
