ef90c1d23eec9a4103cf7bac9a9f2a6530231706
Guy Ellis <wildfiction@gmail.com>
2018-01-18 09:41:47 -0700

    Complete test coverage for Mongo db helper. Issue #7

--- test/lib/db/mongo/helper.test.js
+++ test/lib/db/mongo/helper.test.js
@@ -1,9 +1,7 @@
 const Helper = require('../../../../lib/db/mongo/helper');
 
-const logger = require('../../../../lib/logging/logger').create('test.mongo-helper');
-
 describe('/lib/db/mongo/helper', () => {
-  describe('removeEmtpy', () => {
+  describe('removeEmpty', () => {
     test('should remove empty string values', () => {
       const doc = {
         one: 'one',
@@ -12,12 +10,42 @@ describe('/lib/db/mongo/helper', () => {
         four: false,
       };
       const rDoc = Helper.removeEmpty(doc);
-      logger.trace('rDoc:', { rDoc });
+
       expect(rDoc).toEqual({
         one: 'one',
         three: 0,
         four: false,
       });
+    });
+
+    test('should remove empty string values from array', () => {
+      const doc = [{
+        one: 'one',
+        two: '',
+        three: 0,
+        four: false,
+      }, {
+        one: 'one',
+        two: '',
+        three: 0,
+        four: false,
+      }];
+      const rDoc = Helper.removeEmpty(doc);
+
+      expect(rDoc).toEqual([{
+        one: 'one',
+        three: 0,
+        four: false,
+      }, {
+        one: 'one',
+        three: 0,
+        four: false,
+      }]);
+    });
+
+    test('should do nothing if param is falsy', () => {
+      const rDoc = Helper.removeEmpty();
+      expect(rDoc).toBeUndefined();
     });
   });
 });
