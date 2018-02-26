e9af4ee4a7ab90f8b9fee017cbae3de2f9710b96
Guy Ellis <wildfiction@gmail.com>
2018-02-25 21:34:08 -0700

    Use a copy of Location to create a template for Metrics. Issue #704

---
+++ app/components/location/Metrics.jsx
@@ -0,0 +1,250 @@
+// Used to show a collection of metrics for a location.
+// Url: /metrics/<location-name>/_location_id
+
+const Base = require('../base/Base');
+const CircularProgress = require('material-ui/CircularProgress').default;
+const InputComboText = require('../common/InputComboText');
+const PlantItem = require('../plant/PlantItem');
+const React = require('react');
+const { canEdit } = require('../../libs/auth-helper');
+const actions = require('../../actions');
+const NoteCreate = require('../note/NoteCreate');
+const utils = require('../../libs/utils');
+const AddPlantButton = require('../common/AddPlantButton');
+const PropTypes = require('prop-types');
+const { withRouter } = require('react-router-dom');
+
+// TODO: AT THIS POINT THIS FILE IS JUST A COPY OF THE Location file/class
+// with the class renamed to Metrics. It's tested and works on the url.
+// With this as an initial commit it is easy to iterate on this.
+
+class Metrics extends React.Component {
+  static contextTypes = {
+    store: PropTypes.object.isRequired,
+  };
+
+  static renderNoPlants(location, userCanEdit) {
+    return (
+      <Base>
+        <div>
+          {Metrics.renderTitle(location)}
+          <h3 style={{ textAlign: 'center' }}>
+            <div style={{ marginTop: '100px' }}>No plants added yet...</div>
+            <AddPlantButton
+              show={userCanEdit}
+              style={{ marginTop: '10px' }}
+            />
+          </h3>
+        </div>
+      </Base>
+    );
+  }
+
+  static renderWaiting(location) {
+    return (
+      <Base>
+        <div>
+          {Metrics.renderTitle(location)}
+          <h3 style={{ textAlign: 'center' }}>
+            <CircularProgress />
+          </h3>
+        </div>
+      </Base>
+    );
+  }
+
+  static renderTitle(location) {
+    return (
+      <h2 style={{ textAlign: 'center' }}>{`${location.title} - Plant List`}</h2>
+    );
+  }
+
+  static addPlantButton(userCanEdit) {
+    return (
+      <div style={{ float: 'right', marginBottom: '60px' }}>
+        <AddPlantButton
+          show={userCanEdit}
+        />
+      </div>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.postSaveSuccessCreateNote = this.postSaveSuccessCreateNote.bind(this);
+    this.state = { filter: '' };
+  }
+
+  componentWillMount() {
+    const { store } = this.context;
+    const { locations = {} } = store.getState();
+
+    const { id: locationId } = this.props.match.params;
+
+    const plantIds = locations[locationId] && locations[locationId].plantIds;
+    if (!plantIds) {
+      store.dispatch(actions.loadPlantsRequest(locationId));
+    }
+    this.onChange();
+    this.unsubscribe = store.subscribe(this.onChange);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onChange() {
+    const { store } = this.context;
+    const {
+      interim,
+      locations,
+      plants: allLoadedPlants,
+      user: authUser,
+      users,
+    } = store.getState();
+    const state = {
+      allLoadedPlants,
+      authUser,
+      interim,
+      locations,
+      users,
+    };
+    this.setState(state);
+  }
+
+  postSaveSuccessCreateNote() {
+    const { store } = this.context;
+    store.dispatch(actions.editNoteClose());
+  }
+
+  render() {
+    const { store } = this.context;
+    const {
+      filter = '',
+      locations,
+      allLoadedPlants,
+      interim,
+      authUser,
+    } = this.state || {};
+
+    const location = locations && locations[this.props.match.params.id];
+    if (!location) {
+      return (
+        <Base>
+          <div>
+            <CircularProgress />
+          </div>
+        </Base>
+      );
+    }
+
+    const {
+      note: interimNote,
+      plant: plantCreateNote,
+    } = interim.note || {};
+    const createNote = !!interimNote && interimNote.isNew;
+
+    const userCanEdit = canEdit(authUser._id, location);
+    const { _id: locationId } = location;
+
+    if (createNote && userCanEdit) {
+      const style = {
+        paddingTop: '30px',
+        textAlign: 'center',
+      };
+      return (
+        <Base>
+          <div>
+            <h4 style={style}>{`Create a Note for ${plantCreateNote.title}`}</h4>
+            <NoteCreate
+              dispatch={store.dispatch}
+              userCanEdit={userCanEdit}
+              interimNote={interimNote}
+              plant={plantCreateNote}
+              plants={allLoadedPlants}
+              postSaveSuccess={this.postSaveSuccessCreateNote}
+              locationId={locationId}
+            />
+          </div>
+        </Base>
+      );
+    }
+
+    const { plantIds = [] } = location;
+    if (!plantIds.length) {
+      if (interim.loadPlantRequest) {
+        return Metrics.renderWaiting(location);
+      }
+      return this.renderNoPlants(location, userCanEdit);
+    }
+
+    const sortedPlantIds = utils.filterSortPlants(plantIds, allLoadedPlants, filter);
+    const plantStats = utils.plantStats(plantIds, allLoadedPlants);
+
+    // Don't send the name into PlantItem to skip the subtitle
+    // If all the plants are at the same location then don't need the
+    // location name. If the plants are from a search result then send
+    // in the name:
+    // title={location.title}
+    const tileElements = sortedPlantIds.reduce((acc, plantId) => {
+      const plant = allLoadedPlants[plantId];
+      if (plant) {
+        const { _id } = plant;
+        acc.found.push(<PlantItem
+          key={_id}
+          dispatch={store.dispatch}
+          createNote={this.createNote}
+          userCanEdit={userCanEdit}
+          plant={plant}
+        />);
+      } else {
+        acc.unloaded.push(plantId);
+      }
+      return acc;
+    }, { found: [], unloaded: [] });
+
+    if (tileElements.unloaded.length) {
+      store.dispatch(actions.loadUnloadedPlantsRequest(tileElements.unloaded));
+    }
+
+    const filterInput = (<InputComboText
+      changeHandler={e => this.setState({ filter: e.target.value.toLowerCase() })}
+      id="plant-title-filter"
+      label="Filter"
+      name="filter"
+      placeholder="Type a plant name to filter..."
+      value={filter}
+    />);
+
+    const stats = (
+      <div>
+        <p>{`Total: ${plantStats.total}`}</p>
+        <p>{`Alive: ${plantStats.alive}`}</p>
+      </div>);
+
+    return (
+      <Base>
+        <div>
+          {Metrics.renderTitle(location)}
+          {stats}
+          {filterInput}
+          {tileElements.found}
+          {Metrics.addPlantButton(userCanEdit)}
+          <div className="clear">&nbsp;</div>
+        </div>
+      </Base>
+    );
+  }
+}
+
+Metrics.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+module.exports = withRouter(Metrics);
--- app/main.js
+++ app/main.js
@@ -30,6 +30,7 @@ const ReactDOM = require('react-dom');
 const store = require('./store');
 const Terms = require('./components/info/Terms');
 const Location = require('./components/location/Location');
+const Metrics = require('./components/location/Metrics');
 const Locations = require('./components/location/Locations');
 const Users = require('./components/user/Users');
 const poly = require('./poly');
@@ -54,21 +55,22 @@ const routes = (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/auth/token" component={Auth} />
+      <Route path="/article/:slug/:id" component={Article} />
       <Route path="/debug-settings" component={DebugSettings} />
       <Route path="/help" component={Help} />
       <Route path="/layout/:slug/:id" component={LayoutMap} />
-      <Route path="/locations" exact component={Locations} />
       <Route path="/location/:slug/:id" component={Location} />
+      <Route path="/locations" exact component={Locations} />
       <Route path="/locations/:slug/:id" component={Locations} />
-      <Route path="/users" component={Users} />
       <Route path="/login" component={Login} />
+      <Route path="/metrics/:slug/:id" component={Metrics} />
       <Route path="/plant" exact component={Plant} />
       <Route path="/plant/:slug/:id" component={Plant} />
-      <Route path="/article/:slug/:id" component={Article} />
       <Route path="/plants/:slug/:id" component={Plants} />
       <Route path="/privacy" component={Privacy} />
       <Route path="/profile" component={Profile} />
       <Route path="/terms" component={Terms} />
+      <Route path="/users" component={Users} />
       <Redirect to="/help" />
     </Switch>
   </BrowserRouter>
