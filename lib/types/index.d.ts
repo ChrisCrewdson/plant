1ceda2a9602035f01ecd3f9c545b38caefecdfdc
Guy Ellis <wildfiction@gmail.com>
2018-09-01 06:56:14 -0700

    First pass through route files for type fixing. Issue #1969

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -18,6 +18,7 @@ declare namespace Express {
       _id: String;
     };
     files?: Array<Multer.File>;
+    logout?: Function;
   }
 }
 
@@ -33,28 +34,3 @@ declare type Role =
   'owner' |
   'manager' |
   'member';
-
-interface NoteImageSize {
-  name: ImageSizeName; // name of the size, e.g. thumb, sm, md, lg, xl
-  width: Number; // (int32) pixels wide, e.g. 100, 500, 1000, 1500, 2000
-}
-
-interface NoteImage {
-  id: String; // MongoId that corresponds to the name of the file in S3
-  ext: String; // file extension e.g. jpg, png
-  originalname: String; // original name of the file when it was uploaded
-  size: Number; // size in bytes of the original file
-  sizes: Array<NoteImageSize>; // an array of sizes
-}
-
-interface Note {
-  _id: String;
-  date: Number;
-  note: String 
-  plantIds: Array<String>;
-  userId: String;
-  images?: Array<NoteImage>; // An array of images
-  metrics?: Object; // - an object with key/value pairs. Values are numbers or boolean.
-  //- See the app/libs/utils.js file for possible keys in this object and the data types
-}
-
---
+++ lib/types/note.d.ts
@@ -0,0 +1,25 @@
+
+interface NoteImageSize {
+  name: ImageSizeName; // name of the size, e.g. thumb, sm, md, lg, xl
+  width: Number; // (int32) pixels wide, e.g. 100, 500, 1000, 1500, 2000
+}
+
+interface NoteImage {
+  id: String; // MongoId that corresponds to the name of the file in S3
+  ext: String; // file extension e.g. jpg, png
+  originalname: String; // original name of the file when it was uploaded
+  size: Number; // size in bytes of the original file
+  sizes: Array<NoteImageSize>; // an array of sizes
+}
+
+interface Note {
+  _id: String;
+  date: Number;
+  note: String 
+  plantIds: Array<String>;
+  userId: String;
+  images?: Array<NoteImage>; // An array of images
+  metrics?: Object; // - an object with key/value pairs. Values are numbers or boolean.
+  //- See the app/libs/utils.js file for possible keys in this object and the data types
+}
+
