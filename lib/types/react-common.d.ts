fa72f9d317c06895c8d115eae9d18d36013cbcff
Guy Ellis <wildfiction@gmail.com>
2018-09-01 21:41:45 -0700

    Types in the lib/render code. Issue #1969

--- lib/types/react-common.d.ts
+++ lib/types/react-common.d.ts
@@ -1,3 +1,16 @@
 interface NavbarProps {
 
 }
+
+interface OpenGraphMeta {
+  property: String;
+  content: String;
+}
+
+interface ServerSideRenderData {
+  html?: String;
+  initialState?: Object;
+  og?: Array<OpenGraphMeta>; // Facebook Open Graph
+  req?: import('express').Request | Object;
+  title?: String;
+}
