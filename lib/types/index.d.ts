713819a2546aa3d21453aeb5319a2fce14d014d3
Guy Ellis <wildfiction@gmail.com>
2018-11-22 07:54:45 -0700

    More typings on routes issue #1969

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -41,10 +41,36 @@ interface Geo {
   coordinates: GeoCoords;
 }
 
+/**
+ * An interface mapping string indexes to string values in an object
+ */
 interface StringStringObject {
   [key: string]: string;
 }
 
 interface AwsKey {
   Key: string;
+}
+
+interface UploadFile {
+  multerFile: Express.Multer.File;
+  noteFile: NoteImage;
+}
+
+interface UploadFileData {
+  files: DerivedMulterFile[];
+  noteid: string;
+  userid: string;
+}
+
+interface UploadedNoteFile {
+  id: string;
+  ext: string;
+  originalname: string;
+  size: number;
+}
+
+interface DerivedMulterFile {
+  multerFile: Express.Multer.File;
+  noteFile: UploadedNoteFile;
 }
