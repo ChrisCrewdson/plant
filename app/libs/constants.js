730daeb12538eb19d4b7b66d9ad8686a59e829d2
Guy Ellis <wildfiction@gmail.com>
2018-12-26 21:30:42 -0700

    Fix typings in app/lib/constants. Issue #1969

--- app/libs/constants.js
+++ app/libs/constants.js
@@ -1,25 +1,21 @@
+// A file for constants used on both the client and server
+
+/** @type {ImageSizeName[]} */
+const imageSizeNames = ['orig', 'xl', 'lg', 'md', 'sm', 'thumb'];
+
+// Any user that is a member of a location should have one of these
+// roles for that location. The roles are, from left to right, most
+// permissive to least permissive.
+/** @type {Role[]} */
+const roles = ['owner', 'manager', 'member'];
+
 module.exports = {
   uuidRE: /^[0-9a-f]{32}$/i,
-  // Mongo _id regex: 57acfcf5ffaf0524572d58e8
-  mongoIdRE: /^[0-9a-f]{24}$/i,
+  mongoIdRE: /^[0-9a-f]{24}$/i, // Mongo _id regex: 57acfcf5ffaf0524572d58e8
   maxImageFilesPerUpload: 100,
   awsBucketName: 'i.plaaant.com',
-  /**
-  * @type {ImageSizeName[]}
-  */
-  imageSizeNames: ['orig', 'xl', 'lg', 'md', 'sm', 'thumb'],
-  /**
-  * @type {number}
-  */
+  imageSizeNames,
   gisMultiplier: 10 ** 7,
-
-  // Any user that is a member of a location should have one of these
-  // roles for that location. The roles are, from left to right, most
-  // permissive to least permissive.
-  /**
-  * @type {Role[]}
-  */
-  roles: ['owner', 'manager', 'member'],
-
+  roles,
   SERVICE_NAME: 'plant',
 };
