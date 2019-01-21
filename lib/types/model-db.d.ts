4724348bd1e006c6b8323eb0e2a6c85ddd450061
Guy Ellis <wildfiction@gmail.com>
2019-01-21 06:18:20 -0700

    Fix typings for DB Updates. Issue #1969

--- lib/types/model-db.d.ts
+++
@@ -1,8 +0,0 @@
-// Helper shapes for DB
-
-// This is what an Update via the Mongo driver result returns:
-interface UpdateWriteOpResultResult {
-  ok: number;
-  n: number;
-  nModified: number;
-}
