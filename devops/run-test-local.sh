4ba76830aca759844227050d1b1c40ed46b83474
Guy Ellis <wildfiction@gmail.com>
2019-02-15 06:31:32 -0700

    Change precom pre-commit hook to run a different test script that starts the DB in Docker first

---
+++ devops/run-test-local.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script can be used to start the server
+
+# If you have Docker installed then this will setup MongoDB for you.
+# If you don't have Docker and already have MongoDB installed then comment out these lines
+# If the container is already running then this will print a message that can be ignored.
+# docker pull mongo:latest
+# docker run -d -p 27017:27017 --name mongodb mongo
+docker start mongo4
+# docker ps
+
+# Set to relevant environment
+export NODE_ENV=development
+
+# You can comment out the DEBUG line below if you want fewer terminal messages
+# export DEBUG=plant:*
+export PLANT_DB_URL=127.0.0.1:27017
+export PLANT_DB_NAME=plant-test
+
+jest
