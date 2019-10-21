672f66636ac56b30528fe630fc02c475d7e2e43d
Guy Ellis <wildfiction@gmail.com>
2019-10-20 18:57:34 -0700

    Add reminder comment why non-import/export modules are available globally. Issue #2883

--- lib/types/model-plant.ts
+++ lib/types/model-plant.ts
@@ -1,3 +1,10 @@
+/*
+In TypeScript, just as in ECMAScript 2015, any file containing a top-level import or export
+is considered a module. Conversely, a file without any top-level import or export declarations
+is treated as a script whose contents are available in the global scope (and therefore to
+modules as well).
+*/
+
 // There are 3 data models for each collection:
 // 1. Db<Collection> - Represents what is in the database
 // 2. Biz<Collection> - What is passed around on the server
