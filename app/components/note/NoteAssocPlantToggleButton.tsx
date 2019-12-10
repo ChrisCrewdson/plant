53e74e027a6b2f37a7b15db1d42cea23c77449d7
Guy Ellis <wildfiction@gmail.com>
2019-12-09 21:40:22 -0700

    Convert NoteAssocPlantToggleButton from class to Function. Issue #3215

--- app/components/note/NoteAssocPlantToggleButton.tsx
+++ app/components/note/NoteAssocPlantToggleButton.tsx
@@ -21,59 +21,50 @@ interface NoteAssocPlantToggleButtonProps {
   toggleFunc: (id: string) => void;
 }
 
-export default class NoteAssocPlantToggleButton extends React.PureComponent {
-  props!: NoteAssocPlantToggleButtonProps;
+/**
+ * The 3 colors that I settled on for associated a note with plants are:
+ * Selected - primary color - blue - this means that the plant is tagged on this note
+ * Alive - default color - transparent/white - the plant is not tagged and is alive
+ * Dead - secondary color - red/pink/orange - the plant is dead and not tagged.
+ */
+const colors: Record<NoteAssocPlantToggleButtonType, MaterialCore.PropTypes.Color> = {
+  alive: 'default',
+  dead: 'secondary',
+  selected: 'primary',
+};
 
-  /**
-   * The 3 colors that I settled on for associated a note with plants are:
-   * Selected - primary color - blue - this means that the plant is tagged on this note
-   * Alive - default color - transparent/white - the plant is not tagged and is alive
-   * Dead - secondary color - red/pink/orange - the plant is dead and not tagged.
-   */
-  colors: Record<NoteAssocPlantToggleButtonType, MaterialCore.PropTypes.Color> = {
-    alive: 'default',
-    dead: 'secondary',
-    selected: 'primary',
-  };
-
-  static propTypes = {
-    _id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    selectState: PropTypes.string.isRequired,
-    style: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-    toggleFunc: PropTypes.func.isRequired,
-  };
-
-  constructor(props: NoteAssocPlantToggleButtonProps) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle() {
-    const { toggleFunc, _id } = this.props;
+export default function noteAssocPlantToggleButton(props: NoteAssocPlantToggleButtonProps) {
+  const toggle = () => {
+    const { toggleFunc, _id } = props;
     toggleFunc(_id);
-  }
+  };
 
-  render() {
-    const {
-      label,
-      selectState,
-      style,
-    } = this.props;
-    const color = this.colors[selectState];
+  const {
+    label,
+    selectState,
+    style,
+  } = props;
+  const color = colors[selectState];
 
-    const buttonStyle = { ...style, fontSize: 'medium' };
+  const buttonStyle = { ...style, fontSize: 'medium' };
 
-    return (
-      <Button
-        onClick={this.toggle}
-        color={color}
-        size="large"
-        style={buttonStyle}
-        variant="contained"
-      >
-        {label}
-      </Button>
-    );
-  }
+  return (
+    <Button
+      onClick={toggle}
+      color={color}
+      size="large"
+      style={buttonStyle}
+      variant="contained"
+    >
+      {label}
+    </Button>
+  );
 }
+
+noteAssocPlantToggleButton.propTypes = {
+  _id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  selectState: PropTypes.string.isRequired,
+  style: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  toggleFunc: PropTypes.func.isRequired,
+};
