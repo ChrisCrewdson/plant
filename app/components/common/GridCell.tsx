3f303a7ab0a3bba0e055bc85b2a5d1852d0eef3a
Guy Ellis <wildfiction@gmail.com>
2019-12-13 10:26:27 -0700

    Convert GridCell from class to function. Issue #3215

--- app/components/common/GridCell.tsx
+++ app/components/common/GridCell.tsx
@@ -24,79 +24,49 @@ interface GridCellProps {
   value: string|boolean;
 }
 
-export default class GridCell extends React.Component {
-  props!: GridCellProps;
-
-  static propTypes = {
-    editCell: PropTypes.func.isRequired,
-    editId: PropTypes.string,
-    error: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    options: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-    rowId: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    value: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.bool,
-    ]).isRequired,
-  };
-
-  static defaultProps = {
-    editId: '',
-    options: {},
-  };
-
-  constructor(props: GridCellProps) {
-    super(props);
-    this.onChangeText = this.onChangeText.bind(this);
-    this.onChangeBoolean = this.onChangeBoolean.bind(this);
-    this.onChangeSelect = this.onChangeSelect.bind(this);
-  }
+export default function gridCell(props: GridCellProps) {
+  const {
+    editCell,
+    editId,
+    error,
+    index,
+    options,
+    rowId,
+    title,
+    type,
+    value,
+  } = props;
 
   /**
    * Change Handler for Select
    */
-  onChangeSelect(event: React.ChangeEvent<{ value: unknown }>) {
+  const onChangeSelect = (event: React.ChangeEvent<{ value: unknown }>) => {
     const val = event.target.value as string;
-    const {
-      editCell, rowId, index,
-    } = this.props;
     editCell(rowId, index, val);
-  }
+  };
 
-  onChangeText(name: string, val: string) {
-    const {
-      editCell, rowId, index,
-    } = this.props;
+  const onChangeText = (name: string, val: string) => {
     editCell(rowId, index, val);
-  }
+  };
 
-  onChangeBoolean(name: string, val: boolean) {
-    const {
-      editCell, rowId, index,
-    } = this.props;
+  const onChangeBoolean = (name: string, val: boolean) => {
     editCell(rowId, index, val);
-  }
+  };
 
-  render() {
-    const {
-      editId, rowId, value, type, title, options, error, index,
-    } = this.props;
-    const htmlId = `${editId}-${index}`;
+  const htmlId = `${editId}-${index}`;
 
-    // If we're in edit mode for this cell then return an edit component
-    // Edit mode is defined as the condition below
-    if (editId === rowId) {
-      if (type === 'select' && options) {
-        return (
-          <Select
-            id={htmlId}
-            value={value}
-            onChange={this.onChangeSelect}
-            style={{ marginLeft: 20 }}
-          >
-            {
+  // If we're in edit mode for this cell then return an edit component
+  // Edit mode is defined as the condition below
+  if (editId === rowId) {
+    if (type === 'select' && options) {
+      return (
+        <Select
+          id={htmlId}
+          value={value}
+          onChange={onChangeSelect}
+          style={{ marginLeft: 20 }}
+        >
+          {
             Object.keys(options).map((key) => (
               <MenuItem
                 key={key}
@@ -106,62 +76,81 @@ export default class GridCell extends React.Component {
               </MenuItem>
             ))
           }
-          </Select>
-        );
-      }
-      if (type === 'text' && typeof value === 'string') {
-        return (
-          <InputComboText
-            changeHandler={this.onChangeText}
-            error={error}
-            id={htmlId}
-            name={title}
-            placeholder={title}
-            style={{}}
-            type={type}
-            value={value}
-          />
-        );
-      }
-      if (type === 'boolean' && typeof value === 'boolean') {
-        return (
-          <InputCombo
-            booleanChangeHandler={this.onChangeBoolean}
-            name={title}
-            value={value}
-          />
-        );
-      }
-      return null;
+        </Select>
+      );
     }
-
-    // We're in read-only mode for this cell
-
-    // If the type is boolean then show a checkbox div.
-    // value will be typeof boolean
-    if (type === 'boolean' || typeof value === 'boolean') {
-      const checked = !!value;
+    if (type === 'text' && typeof value === 'string') {
       return (
-        <Switch
-          disabled
-          checked={checked}
+        <InputComboText
+          changeHandler={onChangeText}
+          error={error}
+          id={htmlId}
+          name={title}
+          placeholder={title}
+          style={{}}
+          type={type}
+          value={value}
         />
       );
     }
-
-    // Assume that the text to show will be the value
-    let text = value;
-
-    // If the type is "select" then the value is a MongoId prop in the options that
-    // will have the value.
-    if (type === 'select') {
-      text = (options && options[value]) || '';
+    if (type === 'boolean' && typeof value === 'boolean') {
+      return (
+        <InputCombo
+          booleanChangeHandler={onChangeBoolean}
+          name={title}
+          value={value}
+        />
+      );
     }
+    return null;
+  }
 
+  // We're in read-only mode for this cell
+
+  // If the type is boolean then show a checkbox div.
+  // value will be typeof boolean
+  if (type === 'boolean' || typeof value === 'boolean') {
+    const checked = !!value;
     return (
-      <span>
-        {text}
-      </span>
+      <Switch
+        disabled
+        checked={checked}
+      />
     );
   }
+
+  // Assume that the text to show will be the value
+  let text = value;
+
+  // If the type is "select" then the value is a MongoId prop in the options that
+  // will have the value.
+  if (type === 'select') {
+    text = (options && options[value]) || '';
+  }
+
+  return (
+    <span>
+      {text}
+    </span>
+  );
 }
+
+gridCell.propTypes = {
+  editCell: PropTypes.func.isRequired,
+  editId: PropTypes.string,
+  error: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  options: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  rowId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool,
+  ]).isRequired,
+};
+
+gridCell.defaultProps = {
+  editId: '',
+  options: {},
+};
