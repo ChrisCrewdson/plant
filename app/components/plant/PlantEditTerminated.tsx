ae5b299e2f829244ce51893356e208b57db044d4
Guy Ellis <wildfiction@gmail.com>
2019-12-13 06:34:13 -0700

    Convert PlantEditTerminated from class to function. Issue #3215

--- app/components/plant/PlantEditTerminated.tsx
+++ app/components/plant/PlantEditTerminated.tsx
@@ -16,145 +16,132 @@ interface PlantEditTerminatedProps {
   interimPlant: UiPlantsValue;
 }
 
-export default class PlantEditTerminated extends React.Component {
-  props!: PlantEditTerminatedProps;
+export default function plantEditTerminated(props: PlantEditTerminatedProps) {
+  const {
+    interimPlant: {
+      errors = {},
+      isTerminated = false,
+      terminatedDate = '',
+      terminatedDescription = '',
+      terminatedReason: tr,
+    },
+    dispatch,
+  } = props;
 
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    interimPlant: PropTypes.shape({
-      errors: PropTypes.object,
-      isTerminated: PropTypes.bool,
-      terminatedDate: PropTypes.string,
-      terminatedDescription: PropTypes.string,
-      terminatedReason: PropTypes.string,
-    }).isRequired,
+  const terminatedReason = tr ?? 'died';
+
+  if (!tr) {
+    dispatch(actionFunc.editPlantChange({
+      terminatedReason,
+    }));
+  }
+  // }
+
+  const dispatchChange = (name: string, value: string | boolean): void => {
+    dispatch(actionFunc.editPlantChange({ [name]: value }));
   };
-
-  constructor(props: PlantEditTerminatedProps) {
-    super(props);
-    this.onChange = this.onChange.bind(this);
-    this.onChangeRadio = this.onChangeRadio.bind(this);
-    this.booleanHandler = this.booleanHandler.bind(this);
-    this.dispatchChange = this.dispatchChange.bind(this);
-
-    const { dispatch } = this.props;
-    const { interimPlant } = this.props;
-    let { terminatedReason } = interimPlant;
-    if (!terminatedReason) {
-      terminatedReason = 'died';
-      dispatch(actionFunc.editPlantChange({
-        terminatedReason,
-      }));
-    }
-  }
-
-  onChangeRadio(e: React.ChangeEvent<HTMLInputElement>): void {
-    const { name, value } = e.target;
-    this.dispatchChange(name, value);
-  }
 
   /**
    * Change Handler for InputComboText
    */
-  onChange(name: string, value: string): void {
-    this.dispatchChange(name, value);
-  }
-
-  booleanHandler = (event: ChangeEvent<HTMLInputElement>, checked: boolean): void => {
-    const { name } = event.currentTarget;
-    this.dispatchChange(name, checked);
+  const onChange = (name: string, value: string): void => {
+    dispatchChange(name, value);
   };
 
-  dispatchChange(name: string, value: string | boolean): void {
-    const { dispatch } = this.props;
-    dispatch(actionFunc.editPlantChange({ [name]: value }));
-  }
+  const booleanHandler = (event: ChangeEvent<HTMLInputElement>, checked: boolean): void => {
+    const { name } = event.currentTarget;
+    dispatchChange(name, checked);
+  };
 
-  render() {
-    const styles = {
-      radioGroup: {
-        display: 'flex',
-        marginLeft: '20px',
-      },
-    };
+  const onChangeRadio = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    dispatchChange(name, value);
+  };
 
-    const {
-      interimPlant: {
-        errors = {},
-        isTerminated = false,
-        terminatedDate = '',
-        terminatedDescription = '',
-        terminatedReason,
-      },
-    } = this.props;
+  const styles = {
+    radioGroup: {
+      display: 'flex',
+      marginLeft: '20px',
+    },
+  };
 
-    const dateFormat = 'MM/DD/YYYY';
+  const dateFormat = 'MM/DD/YYYY';
 
-    return (
-      <div>
-        <FormControlLabel
-          control={(
-            <Switch
-              checked={isTerminated}
-              color="primary"
-              name="isTerminated"
-              onChange={this.booleanHandler}
-              style={{ paddingLeft: '5px', maxWidth: '150px' }}
-            />
-            )}
-          label="Terminated"
-          labelPlacement="start"
-        />
-        {isTerminated
-          && (
-          <div>
-            <InputComboText
-              changeHandler={this.onChange}
-              error={errors.terminatedDate}
-              id="termination-date"
-              label="Termination Date"
-              name="terminatedDate"
-              placeholder={dateFormat}
-              value={terminatedDate}
-            />
-            <RadioGroup
-              name="terminatedReason"
-              onChange={this.onChangeRadio}
-              row
-              style={styles.radioGroup}
-              value={terminatedReason}
-            >
-              <FormControlLabel
-                control={<Radio />}
-                label="Culled"
-                value="culled"
-              />
-              <FormControlLabel
-                control={<Radio />}
-                label="Died"
-                value="died"
-
-              />
-              <FormControlLabel
-                control={<Radio />}
-                label="Transferred"
-                value="transferred"
-              />
-            </RadioGroup>
-            <InputComboText
-              changeHandler={this.onChange}
-              error={errors.terminatedDescription}
-              id="termination-description"
-              label="Termination Description"
-              name="terminatedDescription"
-              placeholder="(Optional) Describe why this plant was terminated."
-              value={terminatedDescription}
-            />
-
-            <Divider />
-          </div>
+  return (
+    <div>
+      <FormControlLabel
+        control={(
+          <Switch
+            checked={isTerminated}
+            color="primary"
+            name="isTerminated"
+            onChange={booleanHandler}
+            style={{ paddingLeft: '5px', maxWidth: '150px' }}
+          />
           )}
-      </div>
-    );
-  }
+        label="Terminated"
+        labelPlacement="start"
+      />
+      {isTerminated
+        && (
+        <div>
+          <InputComboText
+            changeHandler={onChange}
+            error={errors.terminatedDate}
+            id="termination-date"
+            label="Termination Date"
+            name="terminatedDate"
+            placeholder={dateFormat}
+            value={terminatedDate}
+          />
+          <RadioGroup
+            name="terminatedReason"
+            onChange={onChangeRadio}
+            row
+            style={styles.radioGroup}
+            value={terminatedReason}
+          >
+            <FormControlLabel
+              control={<Radio />}
+              label="Culled"
+              value="culled"
+            />
+            <FormControlLabel
+              control={<Radio />}
+              label="Died"
+              value="died"
+
+            />
+            <FormControlLabel
+              control={<Radio />}
+              label="Transferred"
+              value="transferred"
+            />
+          </RadioGroup>
+          <InputComboText
+            changeHandler={onChange}
+            error={errors.terminatedDescription}
+            id="termination-description"
+            label="Termination Description"
+            name="terminatedDescription"
+            placeholder="(Optional) Describe why this plant was terminated."
+            value={terminatedDescription}
+          />
+
+          <Divider />
+        </div>
+        )}
+    </div>
+  );
 }
+
+plantEditTerminated.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  interimPlant: PropTypes.shape({
+    errors: PropTypes.object,
+    isTerminated: PropTypes.bool,
+    terminatedDate: PropTypes.string,
+    terminatedDescription: PropTypes.string,
+    terminatedReason: PropTypes.string,
+  }).isRequired,
+};
