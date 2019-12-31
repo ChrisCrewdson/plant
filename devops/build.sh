d10dd1dc14386c9de7824c8e89abb22b12a75fa7
Guy Ellis <wildfiction@gmail.com>
2019-12-31 06:33:07 -0700

    Add some comments to build file

--- devops/build.sh
+++ devops/build.sh
@@ -14,10 +14,15 @@ rm -rf dist/public/
 
 # Copy the public/ folder and all its subdirectories to
 # the same in dist/
+# This is because the server is going to start in the dist/
+# folder as its root folder and will look for public/
+# under there.
 cp -R public/ dist/public/
 
 # Remove the existing build/ folder from dist/
 # This will error if it does not exist - that's okay
+# Same reason as above. Root is dist/ so build needs to
+# be in there to provide client bundle file etc.
 rm -rf dist/build/
 
 # Copy the build/ folder and all its subdirectories to
